9edb0a4229856c3210ca91edbcefedb5cef58b82
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-01-25 16:30:04 -0500

    Node 101 small and medium exercises

---
+++ node101/medium-EX/dns_lookup.js
@@ -0,0 +1,17 @@
+const dns = require('dns');
+
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.question("Domain Name: ", function (url) {
+    readline.close();
+    dns.lookup(url, function (error, address) {
+        if (error) {
+            console.log(error.message);
+            return;
+        }
+        console.log("IP Address: ", address);
+    })
+});
---
+++ node101/medium-EX/read_and_write_file.js
@@ -0,0 +1,28 @@
+const FS = require("fs");
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.question("Input file : ", function (inputFile) {
+    FS.readFile(inputFile, function(error, buffer) {
+        if (error) {
+            console.log(error.message);
+            readline.close();
+            return;
+        }
+        readline.question("Output file: ", function(outputFile) {
+            readline.close();
+            const content = buffer.toString();
+            const upperCased = content.toUpperCase();
+            FS.writeFile(outputFile, upperCased, function(error) {
+                if (error) {
+                    console.log(error.message);
+                    return;
+                }
+                console.log("Wrote to file ", outputFile);
+            });
+        });
+    });
+});
