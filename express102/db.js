a74d85e0fa7294490e1e992c3695ecafab02719b
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-01-27 12:30:56 -0500

    refactoring of the express 101

---
+++ express102/db.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        name: 'Luke',
+        handle: '@luke',
+    },
+    {
+        name: 'Ahsoka',
+        handle: '@ahsoka',
+    },
+    {
+        name: 'Han',
+        handle: '@han',
+    },
+    {
+        name: 'Tiana',
+        handle: '@tiana'
+    },
+];
---
+++ express102/routes/greet.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express'),
+    router = express.Router();
+
+    const friendsArray = require('../db');
+
+router.get('/', (req, res) => {
+    let htmlData = `<ul>` 
+    friendsArray.map(friend => {
+        htmlData += `<li><a href="./greet/${friend.handle}">${friend.name}</a></li>`
+    })
+    htmlData += `</ul>`
+    res.send(htmlData)
+});
+
+router.get('/:handle', (req, res) => {
+    const { handle } = req.params;
+    const friend = friendsArray.find((friend) => {
+        if (friend.handle === handle) {
+            return friend;
+        }
+    });
+    if (friend) {
+        res.send(`<h1>Howdy ${friend.name}!</h1>`);
+    } else {
+        res.send(`No friend with the handle, ${handle}, was found. 👎`)
+    }
+})
+
+module.exports = router;
