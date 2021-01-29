a5ba87a00002631a475e957ad5a846e18f5921f3
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-01-28 21:02:17 -0500

    added movies and tv-shows express templates

---
+++ movies_tv_shows/routes/movies.js
@@ -0,0 +1,42 @@
+const express = require('express'),
+    router = express.Router();
+
+const moviesModel = require('../db');
+
+router.get('/', (req, res) => {
+    res.render('template', {
+        locals: {
+            title: 'List of Movies and Tv-Shows',
+            data: moviesModel
+        },
+        partials: {
+            body: 'partials/movie-list',
+        },
+    });
+});
+
+router.get('/:imdbID', (req, res) => {
+    const { imdbID } = req.params;
+    const movie = moviesModel.find((movie) => {
+        if (movie.imdbID === imdbID) {
+            return movie;
+        };
+    });
+    if (movie) {
+        res.render('template', {
+            locals: {
+                title: `${movie.Title}'s info`,
+                movie,
+                Images: movie.Images,
+                Poster: movie.Poster,
+            },
+            partials: {
+                body: 'partials/movie-details',
+            },
+        });
+    } else {
+        res.status(404).send(`No movie or show with the imdbID of ${imdbID}. Sorry 😢`)
+    }
+});
+
+module.exports = router;
